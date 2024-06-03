51aaa28a375a1a2e63fcc3b04bd880804dfea99f
vutrinhtu <vutubtt893@gmail.com>
2024-06-04 00:21:25 +0700

    [FINISH] build function convert to hexa

--- finalExam/bai5.asm
+++ finalExam/bai5.asm
@@ -8,6 +8,8 @@
     str db 256 dup('$')
     deci db 0 
     muoi dw 10
+    cosohexa dw 16
+    
 .code
 MAIN Proc
     mov ax, @data
@@ -36,7 +38,15 @@ MAIN Proc
     xor ax, ax
     mov al, deci
     call print_result
-      
+    
+    ;hien thi ket qua 2
+    mov ah, 9
+    lea dx, msg3
+    int 21h
+    
+    xor ax, ax
+    mov al, deci
+    call convert_to_hexa  
 exit_programing:    
     mov ah, 4ch
     int 21h
@@ -62,16 +72,57 @@ MAIN Endp
     
     convert_binary_to_decima proc
         loop_deci:
-        xor dx, dx
-        mov dl, [si]
-        sub dl, '0'
-        sub cl, 1
-        shl dl, cl
-        add deci, dl
-        inc si
-        inc cl
-        loop loop_deci 
+            xor dx, dx
+            mov dl, [si]
+            sub dl, '0'
+            sub cl, 1
+            shl dl, cl
+            add deci, dl
+            inc si
+            inc cl
+            loop loop_deci 
         ret
-    convert_binary_to_decima endp
+    convert_binary_to_decima endp 
+    
+    convert_to_hexa proc
+        cmp ax, 0000FH
+        jle buffer
+        jmp exit_buffer
+        buffer:
+            mov dx, 0
+            add dx, '0'
+            mov ah, 2
+            int 21h
+        exit_buffer:
+            xor ax, ax
+            mov al, deci
+            xor cx, cx
+        loop_hexa:
+            xor dx, dx
+            div cosohexa
+            push dx
+            inc cx
+            cmp ax, 0
+            jne loop_hexa
+        print_result_hexa:
+            pop dx
+            cmp dx, 0000AH
+            jl print_number
+            jmp print_char
+        print_number:
+            add dx, '0'
+            jmp end_if
+        print_char:
+            add dx, 55
+            jmp end_if
+        end_if:
+            mov ah, 2
+            int 21h
+            loop print_result_hexa
+            jmp end
+        end:
+        ret
+    convert_to_hexa endp
+        
     
 END MAIN             
